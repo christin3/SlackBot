2bc4a8bd4cc50d4a5aacb44df1527ee2e5f82fb0
Christine <baba.christine@gmail.com>
2017-01-20 20:24:54 -0600

    Rhyme test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-    var Slack = require('slack-node');
+var Slack = require('slack-node');
 var express = require('express');
 var url = require('url');
 var app = express();
-
+var request = require('request');
 
 ////////////// THE SETUP ///////////////////////////////////////////
 
@@ -10,9 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
 
-    var urlObject = url.parse(request.url,true).query
+    var urlObject = url.parse(request.url, true).query
     console.log(urlObject)
     sendMessage(urlObject);
 
@@ -21,24 +21,36 @@ app.get('/', function(request, response) {
 
 /////////////// THE SEND MESSAGE //////////////////////////////////////////
 
-function sendMessage(urlObject){
+function sendMessage(urlObject) {
 
     slack = new Slack();
     slack.setWebhook(urlObject.response_url);
 
     //   /mySlashCommand catfish    'catfish' is stored in var userCommand
     var userText = urlObject.text;
+    var userCommand = urlObject.text;
 
-    slack.webhook({
-     channel: urlObject.channel_name,
+// API call
+    request('http://rhymebrain.com/talk?function=getRhymes&word=' + userCommand, function (error, response, body) {
 
-      text: "hello you typed: " + userText                  // the response back to slack
+        var rhymeWord = JSON.parse(body)[0].word;
 
-    }, function(err, response) {
-        if (err){
-            console.log(err)
-        }
-    });
-}
+
+//end API call
+
+
+        slack.webhook({
+            channel: urlObject.channel_name,
+
+            text: "hello you typed: " + userText + rhymeWord
+            // the response back to slack
+
+        }, function (err, response) {
+            if (err) {
+                console.log(err)
+            }
+        })//end webhook
+    })//end request
+}//get user command
 
 /////////////////////////////////////////////////////////
